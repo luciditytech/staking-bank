7c048575437481fcb9cdeba369f112e49153f0fc
DZariusz <DZariusz@gmail.com>
2019-03-15 18:54:56 +0100

    feat(*): add placeholder for manage shards
    Placeholders for manage shards will allow us optimize `StakingBank`.
    I will help emove dependency to `VerifeirRegisty` in a future.

--- contracts/StakingBankStorage.sol
+++ contracts/StakingBankStorage.sol
@@ -12,12 +12,15 @@ contract StakingBankStorage is StorageBase {
   mapping (address => uint256) public stakingBalances;
 
   uint256 public verifiersPerShard;
-  mapping (address => uint256) public verifiersShards;
+  address[] public addresses;
+  // if verifiersShard == 0 means its not assign, first shard number is 1.
+  mapping (address => uint256) public verifiersShard;
   mapping(uint256 => uint256) public stakingBalancesPerShard;
 
   constructor(IERC20 _token) public {
     require(address(_token) != address(0x0), "empty token address");
     token = _token;
+    verifiersPerShard = 100;
   }
 
   function setStakingBalance(address _verifier, uint256 _balance)
@@ -26,11 +29,27 @@ contract StakingBankStorage is StorageBase {
     stakingBalances[_verifier] = _balance;
   }
 
+  function addressesLength() external view returns (uint256) {
+    return addresses.length;
+  }
+
+  function addressesPush(address _addr)
+  external
+  onlyFromStorageOwner {
+    addresses.push(_addr);
+  }
+
   function setVerifiersPerShard(uint256 _verifiersPerShard)
   external
   onlyFromStorageOwner {
     require(_verifiersPerShard > 0, "_verifiersPerShard must be gt 0");
     verifiersPerShard = _verifiersPerShard;
+  }
+
+  function setVerifiersShard(address _verifier, uint256 _shard)
+  external
+  onlyFromStorageOwner {
+    verifiersShard[_verifier] = _shard;
   }
 
   function setStakingBalancePerShard(uint256 _shard, uint256 _balance)
